bc52e106a1e4ee5089a9935052a976d0aa95f6b6
xiapengcheng <aiyucp@qq.com>
2023-04-14 13:33:30 +0800

    feat: 更新依赖

--- packages/apm-web/rollup.config.js
+++ packages/apm-web/rollup.config.js
@@ -6,7 +6,7 @@ import { terser } from "rollup-plugin-terser";
 import nodeResolve from "rollup-plugin-node-resolve";
 import vue from "rollup-plugin-vue";
 import filesize from "rollup-plugin-filesize";
-import babel from "rollup-plugin-babel";
+import babel from "@rollup/plugin-babel";
 import replace from "rollup-plugin-replace";
 import pkg from "./package.json" assert { type: "json" };
 
